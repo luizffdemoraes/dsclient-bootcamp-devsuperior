f0c7e0caf32711df9958fd3ef1043de3fdb522e2
Luiz Moraes <lffm1994@gmail.com>
2021-06-04 03:46:41 -0300

    Method Delete OK

--- dsclient/src/main/java/br/com/lffm/dsclient/services/ClientService.java
+++ dsclient/src/main/java/br/com/lffm/dsclient/services/ClientService.java
@@ -2,15 +2,24 @@ package br.com.lffm.dsclient.services;
 
 import javax.persistence.EntityNotFoundException;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.lffm.dsclient.dto.ClientDTO;
 import br.com.lffm.dsclient.entities.Client;
 import br.com.lffm.dsclient.repositories.ClientRepository;
+import javassist.NotFoundException;
+
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 public class ClientService {
@@ -30,7 +39,7 @@ public class ClientService {
 
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) throws Exception {
-		java.util.Optional<Client> obj = repository.findById(id);
+		Optional<Client> obj = repository.findById(id);
 		Client entity = obj.orElseThrow(() -> new Exception("Entity not found"));
 		return new ClientDTO(entity);
 	}
@@ -40,7 +49,7 @@ public class ClientService {
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
 	}
-	
+
 	@Transactional
 	public ClientDTO update(Long id, ClientDTO dto) throws Exception {
 		try {
@@ -63,7 +72,15 @@ public class ClientService {
 	}
 
 	
-	
-	
+	public void delete(Long id) throws Exception {
+		try {
+			repository.deleteById(id);
+			
+		} catch (EmptyResultDataAccessException e) {
+			throw new RuntimeException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new RuntimeException("Integrity violation");
+		}
+	}
 
 }
