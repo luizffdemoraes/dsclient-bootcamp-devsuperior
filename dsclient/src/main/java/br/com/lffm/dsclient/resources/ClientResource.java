b641e6049872bfb239cbaf7f2df69425a1e159b3
Luiz Moraes <lffm1994@gmail.com>
2021-06-04 02:35:49 -0300

    Get all paged

--- dsclient/src/main/java/br/com/lffm/dsclient/resources/ClientResource.java
+++ dsclient/src/main/java/br/com/lffm/dsclient/resources/ClientResource.java
@@ -3,6 +3,9 @@ package br.com.lffm.dsclient.resources;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,20 +22,34 @@ import br.com.lffm.dsclient.services.ClientService;
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
-	
+
 	@Autowired
 	private ClientService service;
 
 	@PostMapping
-	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) throws Exception{
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) throws Exception {
 		ClientDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
+
+
+	@GetMapping
+	public ResponseEntity<Page<ClientDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "10") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Page<ClientDTO> list = service.finAllPaged(pageRequest);
+		return ResponseEntity.ok().body(list);
+
+	}
+
 }
--- dsclient/src/main/java/br/com/lffm/dsclient/services/ClientService.java
+++ dsclient/src/main/java/br/com/lffm/dsclient/services/ClientService.java
@@ -1,6 +1,8 @@
 package br.com.lffm.dsclient.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import br.com.lffm.dsclient.dto.ClientDTO;
@@ -8,25 +10,37 @@ import br.com.lffm.dsclient.entities.Client;
 import br.com.lffm.dsclient.repositories.ClientRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.sun.el.stream.Optional;
+
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository repository;
 
-
-	
 	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
 		Client entity = new Client();
 		copyDtoToEntity(dto, entity);
-		
+
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
-		
+
 	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) throws Exception {
+		java.util.Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new Exception("Entity not found"));
+		return new ClientDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> finAllPaged(PageRequest pageRequest){
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}
+	
 	
 
 	private void copyDtoToEntity(ClientDTO dto, Client entity) {
@@ -36,17 +50,5 @@ public class ClientService {
 		entity.setBirthDate(dto.getBirthDate());
 		entity.setChildren(dto.getChildren());
 	}
-
-
-	public ClientDTO findById(Long id) throws Exception {
-		java.util.Optional<Client> obj = repository.findById(id);
-		Client entity = obj.orElseThrow(() -> new Exception("Entity not found"));
-		return new ClientDTO(entity);
-	}
-
-	
-	
-	
-	
 
 }
